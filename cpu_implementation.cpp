8084c47d05b98f741b34a75dcd53dcf875e03991
Steve Carlson <StephenCarlson@users.noreply.github.com>
2022-12-18 20:56:34 -0800

    Closer on Sobel filter

--- cpu_implementation.cpp
+++ cpu_implementation.cpp
@@ -18,11 +18,14 @@ int main(int argc,char **argv){
 
     std::cout << "GPU Stereopsis" << std::endl;
 
-    // const CImg<unsigned char> left = CImg<>("reference/im2.png"); // 450, 375
-    // const CImg<unsigned char> right = CImg<>("reference/im6.png");
+    const CImg<unsigned char> left_rgb("reference/im2.png"); // 450, 375
+    const CImg<unsigned char> right_rgb("reference/im6.png");
 
-    const CImg<unsigned char> left("reference/im2.png"); // 450, 375
-    const CImg<unsigned char> right("reference/im6.png");
+    // const CImg<unsigned char> left_rgb("frame000869.jpg");
+    // const CImg<unsigned char> right_rgb("frame000870.jpg");
+    
+    const CImg<float> left = left_rgb.get_RGBtoYCbCr().channel(0);
+    const CImg<float> right = right_rgb.get_RGBtoYCbCr().channel(0);
 
     int width = left.width();
     int height = left.height();
@@ -35,11 +38,15 @@ int main(int argc,char **argv){
     std::cout << width << "," << height << "," << size << std::endl;
 
     
-    CImg<float> output(left.width(), left.height(), 1, 3, 0);
+    // CImg<float> output(left.width(), left.height(), 1, 3, 0);
+    CImg<float> output(left.width(), left.height(), 1, 1, 0);
     
-    int offset = 0;
+    const int offset = 1;
+    const char grad_direction[] = "xy"; // 'x' for Vertical Detection, "xy" for both
 
-    const int w = 1; // Offset from h
+    CImgList<float> test = left.get_gradient(grad_direction,2);
+
+    const int w = 5; // Offset from h
     // for(int y=0; y<height-w; y++){
     for(int y=(w/2); y<height-(w/2); y++){
         // std::cout << i << ":";
@@ -50,24 +57,36 @@ int main(int argc,char **argv){
             // std::cout << x << "," << y << ":" << left(x, y, 0) << "\n";
             
             float sum_R = 0;
-            float sum_G = 0;
-            float sum_B = 0;
+            // float sum_G = 0;
+            // float sum_B = 0;
             // for(int u=0; u<w; u++){
             for(int u=(-w/2); u<((w+1)/2); u++){
                 // for(int v=0; v<w; v++){
                 for(int v=(-w/2); v<((w+1)/2); v++){
-                    sum_R += left(x+u, y+v, 0) * left(x+u+offset, y+v, 0);
-                    sum_G += left(x+u, y+v, 1) * left(x+u+offset, y+v, 1);
-                    sum_B += left(x+u, y+v, 2) * left(x+u+offset, y+v, 2);
+                    // sum_R += left(x+u, y+v, 0) * left(x+u+offset, y+v, 0);
+                    // sum_G += left(x+u, y+v, 1) * left(x+u+offset, y+v, 1);
+                    // sum_B += left(x+u, y+v, 2) * left(x+u+offset, y+v, 2);
+
+                    // sum_R += std::abs( left(x+u, y+v, 0) - right(x+u+offset, y+v, 0) );
+                    // sum_G += std::abs( left(x+u, y+v, 1) - right(x+u+offset, y+v, 1) );
+                    // sum_B += std::abs( left(x+u, y+v, 2) - right(x+u+offset, y+v, 2) );
+                    
+                    float diff = ( left(x+u, y+v, 0) - right(x+u+offset, y+v, 0) );
+                    sum_R += (diff * diff);
                 }
             }
             
             // output(x, y, 0) = left(x, y, 0);
             // output(x, y, 1) = left(x, y, 1);
             // output(x, y, 2) = left(x, y, 2);
-            output(x, y, 0) = sum_R/255.0f; // 273.0f
-            output(x, y, 1) = sum_G/255.0f;
-            output(x, y, 2) = sum_B/255.0f;
+
+            // output(x, y, 0) = sum_R/255.0f;
+            // output(x, y, 1) = sum_G/255.0f;
+            // output(x, y, 2) = sum_B/255.0f;
+
+            output(x, y, 0) = sum_R;
+            // output(x, y, 1) = sum_G;
+            // output(x, y, 2) = sum_B;
         }
         // std::cout << std::endl;
     }
@@ -103,6 +122,15 @@ int main(int argc,char **argv){
 // 
 // 
     // // CImg<unsigned char> output(src.data(), src.width(), src.height());
+    output.normalize(0,255);
+    output.equalize(256);
+    int index = 0;
+    for (CImgList<>::iterator it = test.begin(); it<test.end(); ++it){
+        std::string name = "test" + std::to_string(index) + ".bmp";
+        index++;
+        (*it).save(name.c_str());
+    }
+    // test[1].save("test.bmp");
     output.save("output2.bmp");
     // L2Norm.save("L2.bmp");
 
