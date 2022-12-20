13d17e2902cb08c6977f615977069c3ef48a06dd
Steve Carlson <StephenCarlson@users.noreply.github.com>
2022-12-19 16:11:13 -0800

    NCC Working, needs ArgMax

--- cpu_implementation.cpp
+++ cpu_implementation.cpp
@@ -43,20 +43,34 @@ void uniform_filter(CImg<float>& out, const CImg<float>& img, int width, int hei
     }
 }
 
+// void plane_sweep_ncc(Cimg<float>& out, const CImg<float>& left, const CImg<float> right, int width, int height, int size){
+// 
+// }
+
 
 int main(int argc,char **argv){
 
     std::cout << "GPU Stereopsis" << std::endl;
 
+    const int max_disparity = 50;
+    const int w = 7; // Window Width
+
     // Input Image Options
     const CImg<float> left_rgb("reference/im2.png"); // 450, 375
     const CImg<float> right_rgb("reference/im6.png");
-
     // const CImg<float> left_rgb("images/frame000869.jpg");
     // const CImg<float> right_rgb("images/frame000870.jpg");
-
     // const CImg<float> left_rgb("images/test_case_left.bmp");
     // const CImg<float> right_rgb("images/test_case_right.bmp");
+
+    // Bounds Check and Registration
+    int width = left_rgb.width();
+    int height = left_rgb.height();
+    size_t size = left_rgb.size(); //width*height*sizeof(unsigned char);
+    std::cout << width << "," << height << "," << size << std::endl;
+    if(right_rgb.width() != width || right_rgb.height() != height){
+        throw std::runtime_error("Stereo image dimensions do not match"); 
+    }
 
     // Grayscale
     const CImg<float> left_gray  = left_rgb.get_RGBtoHSV().channel(2).normalize(0,255);
@@ -67,92 +81,61 @@ int main(int argc,char **argv){
     const CImg<float> left_gradient  = left_gray.get_gradient(grad_direction,2)[0];
     const CImg<float> right_gradient = right_gray.get_gradient(grad_direction,2)[0];
 
+    // Mean
+    CImg<float> left_mean(width, height, 1, 1, 0);
+    CImg<float> right_mean(width, height, 1, 1, 0);
+    uniform_filter(left_mean,  left_gray,  width, height, w);
+    uniform_filter(right_mean, right_gray, width, height, w);
+    // std::cout << left_gray.print() << std::endl; // 0 to 255
+
     // Final Assignment
-    const CImg<float> left  = left_gradient;
-    const CImg<float> right = right_gradient;
+    const CImg<float> left  = left_gray  - left_mean;   // left_gradient;
+    const CImg<float> right = right_gray - right_mean; // right_gradient;
 
     // Debugging
     left_rgb.save("debug1.bmp");
     left_gray.save("debug2.bmp");
     left_gradient.save("debug3.bmp");
-    left.save("debug4.bmp");
-
-    // Bounds Check and Print
-    int width = left.width();
-    int height = left.height();
-    size_t size = left.size(); //width*height*sizeof(unsigned char);
-    std::cout << width << "," << height << "," << size << std::endl;
-    if(right.width() != width || right.height() != height){
-        throw std::runtime_error("Stereo image dimensions do not match"); 
-    }
+    left_mean.save("debug4.bmp");
+    // std::cout << left_mean.print() << std::endl;
+    // std::cout << left.print() << std::endl;
+    left.get_normalize(0,255).save("debug5.bmp");
 
 
-
-    // Find the mean of each image
-    CImg<float> mean_L(left.width(), left.height(), 1, 1, 0);
-    CImg<float> mean_R(left.width(), left.height(), 1, 1, 0);
-
-    uniform_filter(mean_L, left_gray,  width, height, 7);
-    uniform_filter(mean_R, right_gray, width, height, 7);
-
-    mean_L.save("debug5.bmp");
-    
-    const int max_disparity = 50;
 
     for(int d=0; d<=max_disparity; d++){
-        CImg<float> output(left.width(), left.height(), 1, 1, 0);
+        CImg<float> output(width, height, 1, 1, 0);
 
-        const int w = 7; // Window Width
         for(int y=(w/2); y<height-(w/2); y++){
             for(int x=(w/2); x<width-(w/2); x++){
                 
-                float sum_R = 0.0f;
-                float sum_G = 0.0f;
-                float sum_B = 0.0f;
+                float sum_N = 0.0f;
+                float sum_D1 = 0.0f;
+                float sum_D2 = 0.0f;
                 for(int u=(-w/2); u<((w+1)/2); u++){
                     for(int v=(-w/2); v<((w+1)/2); v++){
-                        // sum_R += left(x+u, y+v, 0) * left(x+u+offset, y+v, 0);
-                        // sum_G += left(x+u, y+v, 1) * left(x+u+offset, y+v, 1);
-                        // sum_B += left(x+u, y+v, 2) * left(x+u+offset, y+v, 2);
+                        float left_term = left(x+u+d, y+v, 0);
+                        float right_term = right(x+u, y+v, 0);
 
-                        // sum_R += std::abs( left(x+u, y+v, 0) - right(x+u+offset, y+v, 0) );
-                        // sum_G += std::abs( left(x+u, y+v, 1) - right(x+u+offset, y+v, 1) );
-                        // sum_B += std::abs( left(x+u, y+v, 2) - right(x+u+offset, y+v, 2) );
-                        
-                        // sum_L += std::abs( right(x+u, y+v, 0) - left(x+u+d, y+v, 0) );
-
-                        // sum_R += left(x+u+d, y+v, 0);
-                        // sum_G += left(x+u+d, y+v, 1);
-                        // sum_B += left(x+u+d, y+v, 2);
-
-                        sum_R += ( right(x+u, y+v, 0) * left(x+u+d, y+v, 0) ) / 255.0f;
-                        // sum_G += ( right(x+u, y+v, 1) * left(x+u+d, y+v, 1) ) / 255.0f;
-                        // sum_B += ( right(x+u, y+v, 2) * left(x+u+d, y+v, 2) ) / 255.0f;
-
-                        
-                        // std::cout << u << "," << v << std::endl;
-
-                        // sum_L += left(x+u, y+v, 0);
-                        // sum_R += right(x+u, y+v, 0);
-
-                        // float diff = ( right(x+u, y+v, 0) - left(x+u+d, y+v, 0) );
-                        // sum_R += (diff * diff);
+                        sum_N  += ( left_term * right_term ); // / 255.0f;
+                        sum_D1 += ( left_term * left_term );
+                        sum_D2 += ( right_term * right_term );
                     }
                 }
-                sum_R /= (1.0f * w * w);
+                // sum_N  /= (1.0f * w * w);
+                // sum_D1 /= (1.0f * w * w);
+                // sum_D2 /= (1.0f * w * w);
 
-                // sum_G /= (1.0f * w * w);
-                // sum_B /= (1.0f * w * w);
-                // float sum_L = std::sqrt( (sum_R * sum_R) + (sum_G * sum_G) + (sum_B * sum_B) ) / std::sqrt(3.0f);
-                // output(x, y, 0) = sum_L;
-
-                output(x, y, 0) = sum_R;
+                // output(x, y, 0) = sum_D1 / (255.0f * w * w); // 0 to 255
+                // output(x, y, 0) = sum_N/std::sqrt(sum_D1 * sum_D2); // -1.0 to 1.0
+                output(x, y, 0) = ( ( sum_N/(std::sqrt(sum_D1 * sum_D2)) )+ 1.0f ) * (255.0f / 2.0f);
             }
             // std::cout << std::endl;
         }
 
         std::string name = "test_output" + std::to_string(d) + ".bmp";
         // output.equalize(256);
+        std::cout << output.print() << std::endl;
         // output.normalize(0,255);
         output.save(name.c_str());
     }
